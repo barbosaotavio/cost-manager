cc19fd82d82fb54b355550403c6c60848438afc0
otavio <barbosaotavio293@gmail.com>
2024-01-27 21:33:48 -0300

    feat:praticando eventos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css'
-import SayMyName from './components/SayMyName'
-import Pessoa from './components/Pessoa'
-import Frase from './components/Frase'
-import List from './components/List'
+import Evento from './components/Evento'
 
 function App() {
-  const nome = 'otavio'
+
   return (
   <div className= 'App'>
     <h1>Testando Eventos</h1>
+    <Evento numero='1'/>
+    <Evento numero='2'/>
     </div>
    
   )
