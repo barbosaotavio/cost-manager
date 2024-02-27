c57ef5cc74c31c33e3badb280ac44c19289a758a
otavio <barbosaotavio293@gmail.com>
2024-02-27 00:45:09 -0300

    adicionando css

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,15 +1,17 @@
 import {Link} from 'react-router-dom'
+import Styles from './Navbar.module.css'
 function Navbar() {
    
     return(
-        <ul>
-        <li>
+        <ul className={Styles.list}>
+
+        <li className={Styles.item}>
           <Link to= '/'>Home</Link>
         </li>
-        <li>
+        <li className={Styles.item}>
           <Link to= '/Empresa'>Empresa</Link>
         </li>
-        <li>
+        <li className={Styles.item}>
           <Link to= '/Contato'>Contato</Link>
         </li>
       </ul>
